49d2b076aec089ffbe143e7d7b6d1c26863de319
hugoMGSung <personar95@naver.com>
2022-06-27 17:39:24 +0900

    220627 Last Commit

---
+++ raspbian/gpio_main12.py
@@ -0,0 +1,25 @@
+from multiprocessing.connection import deliver_challenge
+import adafruit_dht as dht
+import board
+import time
+
+from pexpect import ExceptionPexpect
+
+device = dht.DHT22(board.D4)
+
+while True:
+    try:
+        temp = device.temperature
+        humid = device.humidity
+        print(f'Temp > {temp:.1f} C / Humidity > {humid:.1f}')
+
+    except RuntimeError as e:
+        print(e.args[0])
+        time.sleep(1.0)
+
+    except Exception as e:
+        device.exit()
+        raise e
+
+    time.sleep(1.0)
+
